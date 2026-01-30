185462670fc55a671ff3912b8e8e6c4bc587f6e5
ksdh2333 <ksdh2333@outlook.com>
2026-01-30 20:02:46 +0800

    style: 在入口文件中函数用不到的参数前加上下划线
    - 在入口文件中函数用不到的参数前加上下划线

--- plugin/server_hibernation/__init__.py
+++ plugin/server_hibernation/__init__.py
@@ -12,7 +12,7 @@ from .commands import register_commands
 # Global plugin instance
 plugin_instance = None
 
-def on_load(server: mcdr.PluginServerInterface, old):
+def on_load(server: mcdr.PluginServerInterface, _old):
     """Plugin load event"""
     global plugin_instance
     plugin_instance = ServerHibernationPlugin(server)
@@ -21,20 +21,20 @@ def on_load(server: mcdr.PluginServerInterface, old):
     # Register commands
     register_commands(server, plugin_instance)
 
-def on_unload(server: mcdr.PluginServerInterface):
+def on_unload(_server: mcdr.PluginServerInterface):
     """Plugin unload event"""
     global plugin_instance
     if plugin_instance:
         plugin_instance.unload()
         plugin_instance = None
 
-def on_player_joined(server: mcdr.PluginServerInterface, player: str, info):
+def on_player_joined(_server: mcdr.PluginServerInterface, player: str, info):
     """Player joined event"""
     global plugin_instance
     if plugin_instance:
         plugin_instance.on_player_joined(player, info)
 
-def on_player_left(server: mcdr.PluginServerInterface, player: str):
+def on_player_left(_server: mcdr.PluginServerInterface, player: str):
     """Player left event"""
     global plugin_instance
     if plugin_instance:
